1294ae7ed00c91bca9766423e82c15e1258fa848
valentina0630 <valentinagonzalezcrm@gmail.com>
2024-03-19 16:53:46 -0500

    subiendo

---
+++ libreria/src/componentes/header/Header.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import './header.css'
+
+function Header() {
+    return (
+        <div>
+            <nav className="navbar navbar-expand-lg bg-body-tertiary">
+                <div className="container-fluid">
+        <img src='Libro_icono.png' className= 'logo' alt='Libro_icono'/>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li className="nav-item">
+                                <a className="nav-link active" aria-current="page" href="#">Inicio</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">Tutoriales</a>
+                            </li>
+
+                            <li className="nav-item">
+                                <a className="nav-link disabled" aria-disabled="true">Referencias</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link disabled" aria-disabled="true">Recursos</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link disabled" aria-disabled="true">Contacto</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link disabled" aria-disabled="true">Registrarse</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link disabled" aria-disabled="true">Iniciar Sesión</a>
+                            </li>
+
+                        </ul>
+
+                        <form className="d-flex" role="search">
+                            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+                            <button className="btn btn-outline-success" type="submit">Search</button>
+                        </form>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    )
+}
+
+export default Header
